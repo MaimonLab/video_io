391d97ba999dc896287b6451eb65a2759e527cbc
tlmohren <tlmohren@nogmail.com>
2022-08-26 15:04:53 -0400

    add apt update to isntall dependencies

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,7 +1,18 @@
-#!/bin/bash
+#!/bin/bash 
+# install_dependencies 
+# video_io 
+
+sudo apt-get update
+sudo apt-get install -y python3-pip
 
 # ubuntu-restricted extras required to play H264 codecs?
 sudo apt-get install ubuntu-restricted-extras
 
 # Codec to play `.avi` with the default movie player.
 sudo apt-get install libdvdnav4  gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly libdvd-pkg
+
+echo ""
+echo "---------------------------------------------"
+echo "Finished installing dependencies for video_io"
+echo "---------------------------------------------"
+echo ""
