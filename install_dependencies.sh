384181983b1c23556e181abe3995d5611329724e
Jazz Weisman <jazzlw@gmail.com>
2024-08-12 14:32:09 -0400

    removed problematic libdvd package

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -8,9 +8,6 @@ sudo apt-get install -y python3-pip
 # ubuntu-restricted extras required to play H264 codecs?
 sudo apt-get install ubuntu-restricted-extras
 
-# Codec to play `.avi` with the default movie player.
-sudo apt-get install libdvdnav4  gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly libdvd-pkg
-
 echo ""
 echo "---------------------------------------------"
 echo "Finished installing dependencies for video_io"
