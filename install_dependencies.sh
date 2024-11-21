47fd207d1f77bfe6fdc897a7c3a9b107be2ade48
James Ryu <39420322+jdryu@users.noreply.github.com>
2024-11-21 12:57:33 -0500

    Update install_dependencies.sh

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -6,7 +6,12 @@ sudo apt-get update
 sudo apt-get install -y python3-pip
 
 # ubuntu-restricted extras required to play H264 codecs?
-sudo apt-get install ubuntu-restricted-extras
+# sudo apt-get install ubuntu-restricted-extras
+sudo apt install ffmpeg
+
+cd ~/maimon_ws/src
+git clone git@github.com:MaimonLab/maimon_classes.git
+pip3 install opencv-python
 
 echo ""
 echo "---------------------------------------------"
